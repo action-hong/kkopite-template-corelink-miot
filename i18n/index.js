babbdfee3fd2dc417d2e4018cab13ed73d03fcdc
kkopitehong <kkopite.hong@smartcorelink.com>
2023-10-13 15:17:24 +0800

    chore: 去掉多于的语言

--- i18n/index.js
+++ i18n/index.js
@@ -1,19 +1,13 @@
 import { LocalizedStrings } from './localized';
 import zh from './zh.json';
 import en from './en.json';
-import ko from './ko.json';
-import zhTW from './zh-tw.json';
-import zhHK from './zh-hk.json';
 
 // 为了代码提示
 let o = zh;
 
 o = new LocalizedStrings({
   en,
-  zh,
-  ko,
-  'zh-tw': zhTW,
-  'zh-hk': zhHK
+  zh
 });
 
 export function t(key, ...args) {
